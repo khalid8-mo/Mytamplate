21314e237850cf5c3937d57aecc555490b420a5f
khalid8-mo <aljokar014@gmail.com>
2021-07-04 21:58:32 +0200

    Add Dark Them

---
+++ assets/js/app.js
@@ -0,0 +1,209 @@
+/*=========================================================================================
+  File Name: app.js
+  Description: Template related app JS.
+  ----------------------------------------------------------------------------------------
+  Item Name: Vuexy  - Vuejs, HTML & Laravel Admin Dashboard Template
+  Author: Pixinvent
+  Author URL: hhttp://www.themeforest.net/user/pixinvent
+==========================================================================================*/
+window.colors = {
+  solid: {
+    primary: '#7367F0',
+    secondary: '#82868b',
+    success: '#28C76F',
+    info: '#00cfe8',
+    warning: '#FF9F43',
+    danger: '#EA5455',
+    dark: '#4b4b4b',
+    black: '#000',
+    white: '#fff',
+    body: '#f8f8f8'
+  },
+  light: {
+    primary: '#7367F01a',
+    secondary: '#82868b1a',
+    success: '#28C76F1a',
+    info: '#00cfe81a',
+    warning: '#FF9F431a',
+    danger: '#EA54551a',
+    dark: '#4b4b4b1a'
+  }
+};
+(function (window, document, $) {
+  'use strict';
+  var $html = $('html');
+  var $body = $('body');
+  var $textcolor = '#4e5154';
+  var assetPath = '../../../app-assets/';
+   function getCurrentLayout() {
+    var currentLayout = '';
+    if ($html.hasClass('dark-layout')) {
+      currentLayout = 'dark-layout';
+    } else if ($html.hasClass('bordered-layout')) {
+      currentLayout = 'bordered-layout';
+    } else if ($html.hasClass('semi-dark-layout')) {
+      currentLayout = 'semi-dark-layout';
+    } else {
+      currentLayout = 'light-layout';
+    }
+    return currentLayout;
+  }
+
+  // Get the data layout, for blank set to light layout
+  var dataLayout = $html.attr('data-layout') ? $html.attr('data-layout') : 'light-layout';
+
+  // Navbar Dark / Light Layout Toggle Switch
+  $('.nav-link-style').on('click', function () {
+    debugger
+    var currentLayout = getCurrentLayout(),
+      switchToLayout = '',
+      prevLayout = localStorage.getItem(dataLayout + '-prev-skin', currentLayout);
+
+    // If currentLayout is not dark layout
+    if (currentLayout !== 'dark-layout') {
+      // Switch to dark
+      switchToLayout = 'dark-layout';
+    } else {
+      // Switch to light
+      switchToLayout = prevLayout ? prevLayout : 'light-layout';
+    }
+    // Set Previous skin in local db
+    localStorage.setItem(dataLayout + '-prev-skin', currentLayout);
+    // Set Current skin in local db
+    localStorage.setItem(dataLayout + '-current-skin', switchToLayout);
+
+    // Call set layout
+    setLayout(switchToLayout);
+
+    // ToDo: Customizer fix
+    $('.horizontal-menu .header-navbar.navbar-fixed').css({
+      background: 'inherit',
+      'box-shadow': 'inherit'
+    });
+    $('.horizontal-menu .horizontal-menu-wrapper.header-navbar').css('background', 'inherit');
+  });
+
+  // Get current local storage layout
+  var currentLocalStorageLayout = localStorage.getItem(dataLayout + '-current-skin');
+
+  // Set layout on screen load
+  //? Comment it if you don't want to sync layout with local db
+   setLayout(currentLocalStorageLayout);
+
+  function setLayout(currentLocalStorageLayout) {
+    var navLinkStyle = $('.nav-link-style'),
+      currentLayout = getCurrentLayout(),
+      mainMenu = $('.main-menu'),
+      navbar = $('.header-navbar'),
+      // Witch to local storage layout if we have else current layout
+      switchToLayout = currentLocalStorageLayout ? currentLocalStorageLayout : currentLayout;
+
+    $html.removeClass('semi-dark-layout dark-layout bordered-layout');
+
+    if (switchToLayout === 'dark-layout') {
+      $html.addClass('dark-layout');
+      mainMenu.removeClass('menu-light').addClass('menu-dark');
+      navbar.removeClass('navbar-light').addClass('navbar-dark');
+      navLinkStyle.find('.ficon').replaceWith(feather.icons['sun'].toSvg({ class: 'ficon' }));
+    } else if (switchToLayout === 'bordered-layout') {
+      $html.addClass('bordered-layout');
+      mainMenu.removeClass('menu-dark').addClass('menu-light');
+      navbar.removeClass('navbar-dark').addClass('navbar-light');
+      navLinkStyle.find('.ficon').replaceWith(feather.icons['moon'].toSvg({ class: 'ficon' }));
+    } else if (switchToLayout === 'semi-dark-layout') {
+      $html.addClass('semi-dark-layout');
+      mainMenu.removeClass('menu-dark').addClass('menu-light');
+      navbar.removeClass('navbar-dark').addClass('navbar-light');
+      navLinkStyle.find('.ficon').replaceWith(feather.icons['moon'].toSvg({ class: 'ficon' }));
+    } else {
+      $html.addClass('light-layout');
+      mainMenu.removeClass('menu-dark').addClass('menu-light');
+      navbar.removeClass('navbar-dark').addClass('navbar-light');
+      navLinkStyle.find('.ficon').replaceWith(feather.icons['moon'].toSvg({ class: 'ficon' }));
+    }
+    // Set radio in customizer if we have
+    if ($('input:radio[data-layout=' + switchToLayout + ']').length > 0) {
+      setTimeout(function () {
+        $('input:radio[data-layout=' + switchToLayout + ']').prop('checked', true);
+      });
+    }
+  }
+  $(window).on('load', function () {
+    if (feather) {
+        feather.replace({
+            width: 14,
+            height: 14
+        });
+    }
+})
+})(window, document, jQuery);
+
+// To use feather svg icons with different sizes
+function featherSVG(iconSize) {
+  // Feather Icons
+  if (iconSize == undefined) {
+    iconSize = '14';
+  }
+  return feather.replace({ width: iconSize, height: iconSize });
+}
+
+// jQuery Validation Global Defaults
+if (typeof jQuery.validator === 'function') {
+  jQuery.validator.setDefaults({
+    errorElement: 'span',
+    errorPlacement: function (error, element) {
+      if (
+        element.parent().hasClass('input-group') ||
+        element.hasClass('select2') ||
+        element.attr('type') === 'checkbox'
+      ) {
+        error.insertAfter(element.parent());
+      } else if (element.hasClass('custom-control-input')) {
+        error.insertAfter(element.parent().siblings(':last'));
+      } else {
+        error.insertAfter(element);
+      }
+
+      if (element.parent().hasClass('input-group')) {
+        element.parent().addClass('is-invalid');
+      }
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass('error');
+      if ($(element).parent().hasClass('input-group')) {
+        $(element).parent().addClass('is-invalid');
+      }
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass('error');
+      if ($(element).parent().hasClass('input-group')) {
+        $(element).parent().removeClass('is-invalid');
+      }
+    }
+  });
+}
+
+// Add validation class to input-group (input group validation fix, currently disabled but will be useful in future)
+/* function inputGroupValidation(el) {
+  var validEl,
+    invalidEl,
+    elem = $(el);
+
+  if (elem.hasClass('form-control')) {
+    if ($(elem).is('.form-control:valid, .form-control.is-valid')) {
+      validEl = elem;
+    }
+    if ($(elem).is('.form-control:invalid, .form-control.is-invalid')) {
+      invalidEl = elem;
+    }
+  } else {
+    validEl = elem.find('.form-control:valid, .form-control.is-valid');
+    invalidEl = elem.find('.form-control:invalid, .form-control.is-invalid');
+  }
+  if (validEl !== undefined) {
+    validEl.closest('.input-group').removeClass('.is-valid is-invalid').addClass('is-valid');
+  }
+  if (invalidEl !== undefined) {
+    invalidEl.closest('.input-group').removeClass('.is-valid is-invalid').addClass('is-invalid');
+  }
+} */
